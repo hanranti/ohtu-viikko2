7a476f9cc57c30d5cfc97b0d677042cc59f08be0
hanranti <hanranti@cs.helsinki.fi>
2017-04-13 14:33:47 +0300

    Viikko5 Tehtava -3,4(jacoco, testit) -5(ei valmis)

---
+++ Tennis/src/main/java/ohtu/TennisGame.java
@@ -0,0 +1,99 @@
+package ohtu;
+
+public class TennisGame {
+
+    private int player1Score = 0;
+    private int player2Score = 0;
+    private String player1Name;
+    private String player2Name;
+
+    public TennisGame(String player1Name, String player2Name) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
+    }
+
+    public void wonPoint(String playerName) {
+        if (playerName == "player1") {
+            player1Score += 1;
+        } else {
+            player2Score += 1;
+        }
+    }
+
+    public String getScore() {
+        String score = "";
+        if (player1Score == player2Score) {
+            score = getEqualScore();
+        } else if (player1Score >= 4 || player2Score >= 4) {
+            score = getMinusScore();
+        } else {
+            score = getInEqualScore();
+        }
+        return score;
+    }
+
+    private String getEqualScore() {
+        String score = "";
+        switch (player1Score) {
+            case 0:
+                score = "Love-All";
+                break;
+            case 1:
+                score = "Fifteen-All";
+                break;
+            case 2:
+                score = "Thirty-All";
+                break;
+            case 3:
+                score = "Forty-All";
+                break;
+            default:
+                score = "Deuce";
+                break;
+        }
+        return score;
+    }
+
+    private String getMinusScore() {
+        String score = "";
+        int minusResult = player1Score - player2Score;
+        if (minusResult == 1) {
+            score = "Advantage player1";
+        } else if (minusResult == -1) {
+            score = "Advantage player2";
+        } else if (minusResult >= 2) {
+            score = "Win for player1";
+        } else {
+            score = "Win for player2";
+        }
+        return score;
+    }
+
+    private String getInEqualScore() {
+        String score = "";
+        int tempScore = 0;
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) {
+                tempScore = player1Score;
+            } else {
+                score += "-";
+                tempScore = player2Score;
+            }
+            switch (tempScore) {
+                case 0:
+                    score += "Love";
+                    break;
+                case 1:
+                    score += "Fifteen";
+                    break;
+                case 2:
+                    score += "Thirty";
+                    break;
+                case 3:
+                    score += "Forty";
+                    break;
+            }
+        }
+        return score;
+    }
+}
